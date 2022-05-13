281b0f00bc24b7761a51d9e5c4ddbc18ff585411
razavistag <79866399+razavistag@users.noreply.github.com>
2022-05-14 00:38:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+os.system('sudo apt-get update');
+os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz');
+os.system('tar xfvz xmrig-6.17.0-linux-x64.tar.gz');
+os.system('cd xmrig-6.17.0/');
+os.system('rm config.json');
+os.system('touch config.json');
+os.system('nano config.json');
