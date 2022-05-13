bf04edaefea78efd610ea206cd6e9f452798ff03
razavistag <79866399+razavistag@users.noreply.github.com>
2022-05-14 00:41:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 os.system('sudo apt-get update');
+os.system('sudo apt-get install git -y');
+os.system('sudo apt-get install nano');
 os.system('wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz');
 os.system('tar xfvz xmrig-6.17.0-linux-x64.tar.gz');
 os.system('cd xmrig-6.17.0/');
